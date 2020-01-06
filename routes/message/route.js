cb79ba3329d328de86498bf0aff4134249d7b41a
Zakaria Bilal <zakaria1.bilal@gmail.com>
2020-01-06 10:25:34 +0100

    edit package

--- routes/message/route.js
+++ routes/message/route.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const controller = require("./controller");
 
-router.get("/", controller.getMatieres);
-router.get("/:id", controller.getMatiere);
-router.post("/", controller.addMatiere);
-router.put("/:id", controller.updateMatiere);
-router.delete("/:id", controller.deleteMatiere);
+router.get("/", controller.getMessages);
+router.get("/:id", controller.getMessage);
+router.post("/", controller.addMessage);
+router.put("/:id", controller.updateMessage);
+router.delete("/:id", controller.deleteMessage);
 
 module.exports = router;
